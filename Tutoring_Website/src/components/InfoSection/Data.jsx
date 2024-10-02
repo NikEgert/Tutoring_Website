b029fc0c9bdf0a26a8a9ea25fc2d8fe84554b538
NikEgert <nikita.egert@gmail.com>
2024-10-03 09:57:42 +1000

    working on infosection

--- Tutoring_Website/src/components/InfoSection/Data.jsx
+++ Tutoring_Website/src/components/InfoSection/Data.jsx
@@ -1,3 +1,5 @@
+import img1 from '../../images/svg-3.svg';
+
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
@@ -8,7 +10,7 @@ export const homeObjOne = {
     description: "Whether you're in primary school or just starting out, we teach you the basics of coding using interactive games and simple projects that make learning fun and approachable. Our goal is to help you build confidence and develop problem-solving skills from the very beginning.",
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: '../../images/svg-3.svg',
+    img: img1,
     alt: 'Car',
     dark: true,
     primary: true,
